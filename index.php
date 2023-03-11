fa834aecfd1be487fca9fc270ec8c5667900759f
KELINPATEL0511 <kelin0511@gmail.com>
2023-03-11 17:01:11 +0530

    kelin

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ if(!isset($_SESSION["login"]))
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -39,6 +40,7 @@ if(!isset($_SESSION["login"]))
       <div class="feature3"></div>
       <div class="feature4"></div>
       <div class="feature5"></div>
+      kelin
     </div>
   </div>
   <footer>
